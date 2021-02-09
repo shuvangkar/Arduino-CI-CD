36ded3ae561848e7cbc9a3cc816ecedd02d6703d
shuvangkar <sshuvo93@gmail.com>
2021-02-09 07:31:37 -0500

    git repo download working

--- build-env.sh
+++ build-env.sh
@@ -1,26 +1,36 @@
 # This file creates environment for building the arduino projects
 # sudo apt-get update  
 cd ~ # Navigate to home directory
-
+pwd
 # Install arduino-cli
 # curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 
 # Uninstall AVR core
-# sudo arduino-cli core uninstall arduino:avr -v
+# arduino-cli core uninstall arduino:avr -v
 #Install board
-# sudo arduino-cli core update-index  
-# sudo arduino-cli core install arduino:avr -v
+arduino-cli core update-index  
+# arduino-cli core install arduino:avr -v
 # Configure arduino-cli for the projects
-sudo arduino-cli config set library.enable_unsafe_install true  #enable download library from github
+arduino-cli config set library.enable_unsafe_install true  #enable download library from github
 
 #Install library from github using arduino-cli
 
-# sudo arduino-cli lib install --git-url https://github.com/shuvangkar/FlashMemory.git -v
-# sudo arduino-cli lib install --git-url https://github.com/shuvangkar/realTime.git -v
-# sudo arduino-cli lib install --git-url https://github.com/shuvangkar/RingEEPROM.git -v
+arduino-cli lib install --git-url  https://github.com/shuvangkar/FlashMemory.git       
+arduino-cli lib install --git-url  https://github.com/shuvangkar/realTime.git     
+arduino-cli lib install --git-url  https://github.com/shuvangkar/RingEEPROM.git    
+arduino-cli lib install --git-url  https://github.com/shuvangkar/MQSensor.git
+arduino-cli lib install --git-url  https://github.com/shuvangkar/simple-json.git
+arduino-cli lib install --git-url  https://github.com/shuvangkar/asyncTransfer.git
+arduino-cli lib install --git-url  https://github.com/shuvangkar/asyncXfer.git
+arduino-cli lib install --git-url  https://github.com/shuvangkar/MemQ.git
+arduino-cli lib install --git-url  https://github.com/shuvangkar/ramQ.git
+arduino-cli lib install --git-url  https://github.com/shuvangkar/nRF24.git
 
 # install library using git
-cd ~/Arduino/libraries
-apt-get install git -y
-git clone https://github.com/shuvangkar/FlashMemory.git
-git clone https://github.com/me-no-dev/AsyncTCP.git
+# cd ~/Arduino/libraries
+# pwd
+# apt-get install git -y
+# git clone https://github.com/shuvangkar/FlashMemory.git 
+
+
+
