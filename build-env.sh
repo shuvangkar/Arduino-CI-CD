e5a034a05ca5904f6bbb3b9d97b72cc3eca69338
shuvangkar <sshuvo93@gmail.com>
2021-02-10 01:56:11 -0500

    ssh access added with url format

--- build-env.sh
+++ build-env.sh
@@ -15,16 +15,17 @@ arduino-cli config set library.enable_unsafe_install true  #enable download libr
 
 #Install library from github using arduino-cli
 
-arduino-cli lib install --git-url  https://github.com/shuvangkar/FlashMemory.git       
-arduino-cli lib install --git-url  https://github.com/shuvangkar/realTime.git     
-arduino-cli lib install --git-url  https://github.com/shuvangkar/RingEEPROM.git    
-arduino-cli lib install --git-url  https://github.com/shuvangkar/MQSensor.git
-arduino-cli lib install --git-url  https://github.com/shuvangkar/simple-json.git
-arduino-cli lib install --git-url  https://github.com/shuvangkar/asyncTransfer.git
-arduino-cli lib install --git-url  https://github.com/shuvangkar/asyncXfer.git
-arduino-cli lib install --git-url  https://github.com/shuvangkar/MemQ.git
-arduino-cli lib install --git-url  https://github.com/shuvangkar/ramQ.git
-arduino-cli lib install --git-url  https://github.com/shuvangkar/nRF24.git
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/FlashMemory.git       
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/realTime.git     
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/RingEEPROM.git    
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/MQSensor.git
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/simple-json.git
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/asyncTransfer.git
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/asyncXfer.git
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/MemQ.git
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/ramQ.git
+# arduino-cli lib install --git-url  https://github.com/shuvangkar/nRF24.git
+arduino-cli lib install --git-url  git@github.com:shuvangkar/ramQ.git
 
 
 # install library using git
